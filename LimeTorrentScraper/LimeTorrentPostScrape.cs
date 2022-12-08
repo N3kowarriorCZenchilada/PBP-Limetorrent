143cf3d06e1fb35f74989842371eccda447aab4e
N3kowarriorCZenchilada <118403968+N3kowarriorCZenchilada@users.noreply.github.com>
2022-12-08 09:37:39 -0800

    Add files via upload
    minor changes to make first step work

--- LimeTorrentScraper/LimeTorrentPostScrape.cs
+++ LimeTorrentScraper/LimeTorrentPostScrape.cs
@@ -10,14 +10,28 @@ namespace LimeTorrentScraper
     {
         public static void Main(string[] args)
         {
-            string gameUrl;
-            string tittle;
-            
             string userinput = args[0];
-            ScraperClasses scraperRun = new ScraperClasses();
-            string gameUrlAndTittle = scraperRun.getResaultUrl(userinput);
-            //gameUrl = gameUrlAndTittle.Remove(gameUrlAndTittle.LastIndexOf(dunno)+1);
-            //gameUrl = gameUrl.Remove(0, 1)
+            ScraperClasses searchRun = new ScraperClasses();
+            ScraperClasses scrapeTorrent = new ScraperClasses();
+            ScraperClasses scrapeMagnet = new ScraperClasses();
+
+            //running search
+            string gameUrlAndTittle = searchRun.getResaultUrl(userinput);
+            string gameUrl;
+            string title;
+
+            //Getting url of searched game from string with name and url in ""
+            gameUrl = gameUrlAndTittle.Remove(gameUrlAndTittle.LastIndexOf('"') + 1);
+            gameUrl = gameUrl.Replace(char.ToString('"'), string.Empty);
+            gameUrl = gameUrl.Remove(0, 1);
+            
+            //Getting game title of searched game form string with name and url in""
+            title = gameUrlAndTittle.Substring(gameUrlAndTittle.LastIndexOf('"'));
+            title = title.Remove(0, 1);
+
+
+
+        
         }
     }
 }
--- LimeTorrentScraper/LimeTorrentScraper.cs
+++ LimeTorrentScraper/LimeTorrentScraper.cs
@@ -9,6 +9,7 @@ namespace LimeTorrentScraper
 {
     public class ScraperClasses
     {
+        //Hotovo - Done
         public string getResaultUrl(string userInput)
         {
 
@@ -22,10 +23,33 @@ namespace LimeTorrentScraper
             gameUrlAndTitle = gameUrlAndTitle.Substring(gameUrlAndTitle.Length-4);
             gameUrlAndTitle = gameUrlAndTitle.Remove(0, 8);
             gameUrlAndTitle = gameUrlAndTitle.Replace(">", string.Empty);
-
-            return gameUrlAndTitle; 
+               return gameUrlAndTitle; 
         }
-        
+
+        //V úpravách - In development
+        public string getTorrentUrl(string gameUrl)
+        {
+            string defaultLimeTorrentUrl = "https://www.limetorrents.lol/";
+            string searchUrl = defaultLimeTorrentUrl + gameUrl;
+            HtmlWeb Web = new HtmlWeb();
+            var Doc = Web.Load(searchUrl);
+            var Note = Doc.DocumentNode.SelectNodes("/html/body/div/div[6]/div[1]/div[1]/div[1]/div/div[2]/div/p/a");
+            string torrentLink = Note.ToString();
+                return torrentLink;
+        }
+        public string getMagnetUrl(string gameUrl)
+        {
+            string defaultLimeTorrentUrl = "https://www.limetorrents.lol/";
+            string searchUrl = defaultLimeTorrentUrl + gameUrl;
+            HtmlWeb Web = new HtmlWeb();
+            var Doc = Web.Load(searchUrl);
+            var Note = Doc.DocumentNode.SelectNodes("/html/body/div/div[6]/div[1]/div[1]/div[1]/div/div[4]/div/p/a");
+            string magnetLink = Note.ToString();
+            return magnetLink;
+        }
+
+
+        //LimeTorrent xpaths 
         //xpath 1 Game      /html/body/div/div[6]/div[1]/table[2]/tbody/tr[2]/td[1]/div[1]/a[2]
         //xpath 2 Torrent   /html/body/div/div[6]/div[1]/div[1]/div[1]/div/div[2]/div/p/a
         //xpath 3 Magnet    /html/body/div/div[6]/div[1]/div[1]/div[1]/div/div[4]/div/p/a
