b94e7db88e6fbeb384df936bb44aab97dce7928c
N3kowarriorCZenchilada <118403968+N3kowarriorCZenchilada@users.noreply.github.com>
2022-12-09 17:46:59 -0800

    Add files via upload
    Converted project from net app to command line app.

---
+++ LimeTorrentScraper/LimeTorrentMain.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LimeTorrentScraper
+{
+    public class ProgramClass
+    {
+        public static void Main(string[] args)
+        {
+            string userinput = args[0];
+            ScraperClasses scraperClasses = new ScraperClasses();
+
+            //running search
+            string gameUrlAndTittle = scraperClasses.getResaultUrl(userinput);
+            string gameUrl;
+            string magnetUrl;
+            string torrentUrl;
+            string title;
+
+            //Getting url of searched game from string with name and url in ""
+            gameUrl = gameUrlAndTittle.Remove(gameUrlAndTittle.LastIndexOf('"') + 1);
+            gameUrl = gameUrl.Replace(char.ToString('"'), string.Empty);
+            gameUrl = gameUrl.Remove(0, 1);
+
+            //Getting game title of searched game form string with name and url in""
+            title = gameUrlAndTittle.Substring(gameUrlAndTittle.LastIndexOf('"'));
+            title = title.Remove(0, 1);
+
+            //Getting game's link from game url
+            torrentUrl = scraperClasses.getTorrentUrl(gameUrl);
+            magnetUrl = scraperClasses.getMagnetUrl(gameUrl);
+
+
+
+        }
+    }
+}
