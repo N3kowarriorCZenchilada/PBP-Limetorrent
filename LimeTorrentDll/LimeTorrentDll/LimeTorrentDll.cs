b2c801546c033fb6104be630c2503541c5e3ab1b
N3kowarriorCZenchilada <118403968+N3kowarriorCZenchilada@users.noreply.github.com>
2022-12-14 22:21:49 +0000

    Add files via upload
    minor changes
    >Dll to net 6
    >Dll added missing variables

--- LimeTorrentDll/LimeTorrentDll/LimeTorrentDll.cs
+++ LimeTorrentDll/LimeTorrentDll/LimeTorrentDll.cs
@@ -10,13 +10,24 @@ namespace LimeTorrentScraper
     {
         public string Title => "LimeTorrent";
         public string Type => "Binary";
+        
         public bool isPrefetch => false;
-        public string ScraperPath => "Project Black Pearl\\Extensions\\Scrapers\\LimeTorrentScraper\\LimeTorrentScraper.exe";
+        public string FirstPayloadScraper => "";
+        public string FirstPayloadType => "Binary";
+
+        public string ScraperPath => "Project Black Pearl\\Extensions\\Images\\Scrapers\\LimeTorrent.exe";
         public string URL1Image => "Project Black Pearl\\Extensions\\Images\\TorrentImg.png";
         public string URL1Type => "";
         public string URL1Validator => "";
         public string URL2Image => "Project Black Pearl\\Extensions\\Images\\MagnetImg.png";
         public string URL2Type => "";
         public string URL2Validator => "";
+        public string URL3Image => "";
+        public string URL3Type => "";
+        public string URL3Validator => "";
+        public string URL4Image => "";
+        public string URL4Type => "";
+        public string URL4Validator => "";
+        //Need to lead to actual exe execuatable, add paths
     }
 }
